4d2041deb099c6af5326fcb90f5995fa8bf4c732
BasujitaBhattacharya <121059306+BasujitaBhattacharya@users.noreply.github.com>
2023-01-04 10:06:35 -0500

    Add files via upload

---
+++ src/FrontEnd/JavaWebAppValeyKey/src/com/valeykey/frontend/Constants.java
@@ -0,0 +1,16 @@
+package com.valeykey.frontend;
+
+public final class Constants {
+
+    private Constants() {
+            // restrict instantiation
+    }
+
+    public static final String FUNCTION_URL = "FunctionUrl";
+    public static final String IMAGE_FORMAT = ".jpg";
+    public static final String FQDN = "FQDN";
+    public static final String LOCATION = "Location";
+    public static final String PROTOCOL = "https://";
+    
+    
+}
